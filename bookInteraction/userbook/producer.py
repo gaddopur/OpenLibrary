b54d2672f69b5293f1050d3d05c72b816e56e09c
Aman Singh <amansingh.eee18@itbhu.ac.in>
2022-01-08 13:02:19 +0530

    updated producer file

--- bookInteraction/userbook/producer.py
+++ bookInteraction/userbook/producer.py
@@ -3,12 +3,11 @@
 import pika, json
 
 params = pika.URLParameters('amqps://einowjtq:BFYTVe9jVHse4IBBQIRq_R6l9EnsJwxU@chimpanzee.rmq.cloudamqp.com/einowjtq')
-
 connection = pika.BlockingConnection(params)
 
-channel = connection.channel()
-
-
 def publish(method, body):
+    channel = connection.channel()
+
     properties = pika.BasicProperties(method)
     channel.basic_publish(exchange='', routing_key='bookinteraction', body=json.dumps(body), properties=properties)
+    channel.close()
