83aa19b34c602dab76fff4317d0330a68bc89301
Aman Singh <amansingh.eee18@itbhu.ac.in>
2022-01-09 10:29:46 +0530

    correct connection error

--- userService/users/producer.py
+++ userService/users/producer.py
@@ -3,11 +3,14 @@
 import pika, json
 
 params = pika.URLParameters('amqps://einowjtq:BFYTVe9jVHse4IBBQIRq_R6l9EnsJwxU@chimpanzee.rmq.cloudamqp.com/einowjtq')
-connection = pika.BlockingConnection(params)
+
 
 def publish(method, body):
+    print("Hii I am here")
+    connection = pika.BlockingConnection(params)
     channel = connection.channel()
 
     properties = pika.BasicProperties(method)
     channel.basic_publish(exchange='', routing_key='user', body=json.dumps(body), properties=properties)
     channel.close()
+    connection.close()
